79235c5134d26ceeb6e12466d75f9e2c241f553b
Jayas Piya <pzayazz@gmail.com>
2022-07-01 17:07:59 +0545

    :lipstick: Mobile Responsive Header Nav

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+let drawerOpen = false;
+
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("header").style.paddingTop = "1.5rem";
+//     document.getElementById("header").style.paddingTop = "1.5rem";
+//   } else {
+//     document.getElementById("header").style.height = "4.5rem";
+//   }
+// }
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("toggle");
+  // nav.classList.toggle("nav-toggle");
+});
